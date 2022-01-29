3389ae410f221abfa6904d873298b7525c38ae0c
realtimexx93 <81872364+realtimexx93@users.noreply.github.com>
2022-01-29 12:47:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
